92a41baaeda66b4618720ec94e78b4b90069c40c
Maghsood Salimi <47108647+MaghsoodSalimi@users.noreply.github.com>
2023-12-29 13:07:49 +0100

    Update constscene_segformer.py

--- constscene/constscene_segformer.py
+++ constscene/constscene_segformer.py
@@ -1,5 +1,4 @@
 import argparse
-
 import torch.utils.collect_env
 import torch
 import time
@@ -7,9 +6,7 @@ import logging
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from transformers import SegformerImageProcessor
-
 from tools.prepare_dataset import download_and_unzip
-from tools.seg_tools import write_text_in_file
 from constscene.segmentation_dataset_for_segformer import SemanticSegmentationDataset, SegformerFinetuner
 
 
@@ -33,9 +30,6 @@ def Main():
     parser.add_argument('-d', '--database', default='D1', type=str, choices=('D1', 'D2'),
                         help='database- D1: data without augmentation, D2: data with augmentation')
 
-    parser.add_argument('-lr', '--learning_rate', default=3e-4, type=float,
-                        help='initial learning rate')
-
     parser.add_argument('-b', '--batch_size', default=300, type=int,
                         help='batch size (default: 1500)')
 
@@ -50,13 +44,10 @@ def Main():
 
     args = parser.parse_args()
 
-
-
     # Extracting values from command-line arguments
     epoch_numbers = args.epochs
     data_dir = '../data/' + args.database + '/'
     segformer_model = args.model
-    learning_rate = args.learning_rate
     batch_size = args.batch_size
     logger.info(f'Command-line arguments: {args}')
 
@@ -65,11 +56,8 @@ def Main():
     elif segformer_model == 'b5':
         model_name = "nvidia/segformer-b5-finetuned-ade-640-640"
 
-
     logger.info(f'Checking the input images. (downloading if needed)')
     download_and_unzip(args.database)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     print('Starting .... ')
 
@@ -101,8 +89,6 @@ def Main():
 
     from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 
-    checkpoint_callback = ModelCheckpoint(save_top_k=1, monitor="val_loss")
-
     # saves top-K checkpoints based on "val_loss" metric
     checkpoint_callback = ModelCheckpoint(
         save_top_k=10,
@@ -124,7 +110,6 @@ def Main():
     print('fitting finished. testing started....')
 
     res = trainer.test(ckpt_path="last")
-
 
     print(res)
 
